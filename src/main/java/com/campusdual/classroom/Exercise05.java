df1bbbec55b5d40165b8b74f3a3446e855a6172e
Nando Parga <50nando05@gmail.com>
2025-05-22 10:37:52 +0200

    Exercise05 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -2,6 +2,12 @@ package com.campusdual.classroom;
 
 public class Exercise05 {
     public static void main(String[] args) {
+        System.out.println("Funcion checkLessOrGreaterEqual()");
+        checkLessOrGreaterEqual(2,6);
+        System.out.println("Funcion checkMultiply()");
+        checkMultiply(6,3);
+        System.out.println("Funcion checkPositiveNegative()");
+        checkPositiveNegative(-2);
 
     }
 
@@ -9,20 +15,33 @@ public class Exercise05 {
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
-
+        if (n1 < n2) {
+            System.out.println(n1 + " es menor que " + n2);
+        } else {
+            System.out.println(n1 + " es mayor o igual que " + n2);
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
-    public static void checkMultiply(int n1, int n2) {
-
+    public static void checkMultiply(int n1, int n2){
+        boolean esMultiplo = (n1 % n2 == 0);
+        if (esMultiplo) {
+            System.out.println(n1 + " es multiplo de " + n2);
+        } else {
+            System.out.println(n1 + " no es multiplo de " + n2);
+        }
     }
 
-    //TODO ↓
-    // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
-    // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
-    public static void checkPositiveNegative(int num) {
-        
+            //TODO ↓
+            // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
+            // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
+    public static void checkPositiveNegative ( int num){
+        if (num < 0) {
+            System.out.println(num + " es negativo.");
+        } else {
+                    System.out.println(num + " es positivo.");
+        }
     }
 }
